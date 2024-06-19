eb5531ab3c174c45458696f2b310a731a3965caf
chenyukb <92746141+chenyukb@users.noreply.github.com>
2024-06-19 10:44:30 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("now is on master branch!");
+    System.out.println("add something into dev branch!");
     Map<String, String> envVars = System.getenv();
     for (Map.Entry<String, String> entry : envVars.entrySet()) {
         String key = entry.getKey();
